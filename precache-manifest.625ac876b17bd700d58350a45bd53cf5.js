4fb80fdd63dbddcfc618f556271d501e989a818a
Danil Moroz <danil.moroz@essention.co>
2019-06-10 18:08:04 +0300

    Updates

--- precache-manifest.625ac876b17bd700d58350a45bd53cf5.js
+++ precache-manifest.625ac876b17bd700d58350a45bd53cf5.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "eb4cf4e47e31993a7dffab02273e36c3",
+    "revision": "53d0d89bd5d3f38df8be13b3112dfe8a",
     "url": "/react-breadcrumbs-light/index.html"
   },
   {
-    "revision": "33d32707d0b9b4da3aba",
+    "revision": "67f84620815b3a638890",
     "url": "/react-breadcrumbs-light/static/css/main.b9b4e5a5.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-breadcrumbs-light/static/js/2.0b0f9912.chunk.js"
   },
   {
-    "revision": "33d32707d0b9b4da3aba",
-    "url": "/react-breadcrumbs-light/static/js/main.980feac2.chunk.js"
+    "revision": "67f84620815b3a638890",
+    "url": "/react-breadcrumbs-light/static/js/main.ec335af1.chunk.js"
   },
   {
     "revision": "e0880ae306dc21b1a59f",
