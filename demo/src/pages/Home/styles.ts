b8808d9b48e223dc5618a4ec05d0967b8c968cb0
Danil Moroz <danil.moroz@essention.co>
2019-06-11 04:07:22 +0300

    Update version for demo

--- demo/src/pages/Home/styles.ts
+++ demo/src/pages/Home/styles.ts
@@ -10,7 +10,7 @@ import { Theme } from '@material-ui/core/styles';
 /*                              Styles
 /* ------------------------------------------------------------------- */
 
-export default (theme: Theme) => ({
+export default (theme: Theme): any => ({
   Home: {
     width: '100%',
     height: '100%',
@@ -19,6 +19,8 @@ export default (theme: Theme) => ({
     margin: `${theme.spacing(5)}px auto`,
     padding: theme.spacing(1),
     width: 'fit-content',
+    overflowX: 'scroll !important',
+    maxWidth: '100%',
     // background: theme.palette.common.black,
     background: theme.palette.primary.dark,
     color: theme.palette.common.white
